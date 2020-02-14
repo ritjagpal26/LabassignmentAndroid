9dc857cc076955e42e0ec3806c7598c0e8316a99
Ritik Jagpal <55303144+ritjagpal26@users.noreply.github.com>
2020-02-14 10:08:35 -0500

    all

--- app/src/main/java/com/example/labassignment/Favdapter.java
+++ app/src/main/java/com/example/labassignment/Favdapter.java
@@ -1,6 +1,8 @@
 package com.example.labassignment;
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.view.LayoutInflater;
@@ -20,12 +22,18 @@ public class Favdapter extends ArrayAdapter {
     int layoutRes;
     List<Favplace> favplaces;
     SQLiteDatabase sqLiteDatabase;
+    protected LayoutInflater inflater;
+
     public Favdapter(@NonNull Context mContext, int layoutRes,  List<Favplace> favplaces, SQLiteDatabase sqLiteDatabase) {
         super(mContext, layoutRes, favplaces);
         this.mContext = mContext;
         this.layoutRes = layoutRes;
         this.favplaces = favplaces;
+
         this.sqLiteDatabase = sqLiteDatabase;
+        inflater = (LayoutInflater)mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+
     }
 
     @NonNull
@@ -33,6 +41,7 @@ public class Favdapter extends ArrayAdapter {
     public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
         LayoutInflater inflater = LayoutInflater.from(mContext);
         View v = inflater.inflate(layoutRes,null);
+
         TextView address = v.findViewById(R.id.tvAddress);
 
 
@@ -66,6 +75,24 @@ public class Favdapter extends ArrayAdapter {
 
         }
     }
-
+    private void deleteplace(final Favplace favplace) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(mContext);
+        builder.setTitle("Are you sure?");
+        builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                String sql = "DELETE FROM employees WHERE id = ?";
+                sqLiteDatabase.execSQL(sql, new Integer[]{favplace.getId()});
+                loadfvplaces();
+            }
+        });
+        builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+            }
+        });
+        AlertDialog alertDialog = builder.create();
+        alertDialog.show();
+    }
 
 }
--- app/src/main/java/com/example/labassignment/FavouritePlaces.java
+++ app/src/main/java/com/example/labassignment/FavouritePlaces.java
@@ -1,38 +1,140 @@
 package com.example.labassignment;
 
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
 
+import com.baoyz.swipemenulistview.SwipeMenu;
+import com.baoyz.swipemenulistview.SwipeMenuCreator;
+import com.baoyz.swipemenulistview.SwipeMenuItem;
+import com.baoyz.swipemenulistview.SwipeMenuListView;
+
 import java.util.ArrayList;
 import java.util.List;
 
-public class FavouritePlaces extends AppCompatActivity {
+public class FavouritePlaces extends AppCompatActivity  {
+
     SQLiteDatabase mDatabase;
     List<Favplace> favplaceslist;
-    ListView listView;
+    SwipeMenuListView listView;
+    Favplace favplace;
+    private static final String TAG = "FavouritePlaces";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_favourite_places);
+
         listView = findViewById(R.id.fvPlaces);
         favplaceslist = new ArrayList<>();
         mDatabase = openOrCreateDatabase(MainActivity.DATABASE_NAME, MODE_PRIVATE , null);
 listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
     @Override
     public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        Intent intent = new Intent(FavouritePlaces.this
+                , MainActivity.class);
+        startActivity(intent);
+
 
     }
 });
+
+        SwipeMenuCreator creator = new SwipeMenuCreator() {
+
+            @Override
+            public void create(SwipeMenu menu) {
+                // create "open" item
+                SwipeMenuItem openItem = new SwipeMenuItem(
+                        getApplicationContext());
+                // set item background
+                openItem.setBackground(new ColorDrawable(Color.rgb(255, 255, 255)));
+                // set item width
+                openItem.setWidth(170);
+                // set item title
+                openItem.setTitle("Update");
+                // set item title fontsize
+                openItem.setTitleSize(18);
+                // set item title font color
+                openItem.setTitleColor(Color.BLUE);
+                // add to menu
+                menu.addMenuItem(openItem);
+
+                // create "delete" item
+                SwipeMenuItem deleteItem = new SwipeMenuItem(
+                        getApplicationContext());
+                // set item background
+                deleteItem.setBackground(new ColorDrawable(Color.rgb(255, 255, 255)));
+                // set item width
+                deleteItem.setWidth(170);
+                // set a icon
+                deleteItem.setIcon(R.drawable.ic_action_name);
+                // add to menu
+                menu.addMenuItem(deleteItem);
+            }
+        };
+
+
+        listView.setMenuCreator(creator);
+        listView.setOnMenuItemClickListener(new SwipeMenuListView.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(int position, SwipeMenu menu, int index) {
+                switch (index) {
+                    case 0:
+
+                        AlertDialog.Builder builder = new AlertDialog.Builder(getApplicationContext());
+                        builder.setTitle("Are you sure?");
+                        builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                String sql = "DELETE FROM favplaces WHERE id = ?";
+                                mDatabase.execSQL(sql, new Integer[]{favplace.getId()});
+                                loadfvplaces();
+
+                            }
+                        });
+                        builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                            }
+                        });
+                        AlertDialog alertDialog = builder.create();
+                        alertDialog.show();
+
+                        Log.d(TAG,"On List View Item Clicked " + index);
+
+                        break;
+                    case 1:
+                        Log.d(TAG,"On List View Item Clicked " + index);
+
+                        break;
+                }
+                // false : close the menu; true : not close the menu
+                return false;
+            }
+        });
+
+
         loadfvplaces();
     }
+
+
+// set creator
+
+
+
     private void loadfvplaces() {
         String sql = "SELECT * FROM favplaces";
 
@@ -54,6 +156,7 @@ listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             // show item in a listView
             Favdapter favdapter = new Favdapter(this,R.layout.activity_fav_list_layout,favplaceslist, mDatabase);
             listView.setAdapter(favdapter);
+            favdapter.notifyDataSetChanged();
 
         }
     }
